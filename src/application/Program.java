7a0435d8511175597e063f2fe25af02519edcdd9
Vinícius Dabian <vinicius_dabian@hotmail.com>
2019-11-25 07:19:45 -0300

    Stack trace

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,22 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		method1();
+		
+		System.out.println("End of program!");
+		
+	}
+	
+	public static void method1() {
+		System.out.println("**METHOD2 START**");
+		method2();
+		System.out.println("**METHOD2 END**");
+	}
+	
+	public static void method2() {
+		
+		System.out.println("**METHOD2 START**");
+		
 		Scanner sc = new Scanner (System.in);
 		
 		try {
@@ -16,13 +32,17 @@ public class Program {
 		} 
 		catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position!");
+			e.printStackTrace();
+			sc.next();
 		}
 		catch (InputMismatchException f) {
 			System.out.println("Input error!");
 		}
 		
-		System.out.println("End of program!");
-	
 		sc.close();
+		
+		System.out.println("**METHOD2 END**");
+		
 	}
+	
 }
